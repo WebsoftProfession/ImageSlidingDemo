ba7d59fcd2f62d5043afa1aded52d62b5a12e130
Dotsquares <dotsquares@deepaksharma2.local>
2016-04-06 14:17:12 +0530

    Initial Commit

---
+++ ImageSlidingDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ImageSlidingDemo
+//
+//  Created by Dotsquares on 4/6/16.
+//  Copyright © 2016 Dotsquares. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ImageSlidingDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ImageSlidingDemo
+//
+//  Created by Dotsquares on 4/6/16.
+//  Copyright © 2016 Dotsquares. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
